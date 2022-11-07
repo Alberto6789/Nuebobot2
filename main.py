c9875500e88f97145983b9a8a55d44bd6850c358
pedro1287 <102392245+pedro1287@users.noreply.github.com>
2022-11-06 21:46:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -917,7 +917,7 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 7386053
+            #    api_id = '9951836'
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
             #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
 
